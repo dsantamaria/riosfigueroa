1ee33bb61355c88aac817a069f515c403c747fce
maria <mjcanache@gmail.com>
2017-06-11 19:01:51 -0300

    migration

---
+++ database/migrations/2017_06_11_220020_change_ultima_actualizacion_type_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUltimaActualizacionTypeProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('ultima_actualizacion');
+        });
+        Schema::table('products', function (Blueprint $table) {
+            $table->date('ultima_actualizacion');
+        });  
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('ultima_actualizacion');
+        });
+        Schema::table('products', function (Blueprint $table) {
+            $table->dateTime('ultima_actualizacion');
+        }); 
+    }
+}
