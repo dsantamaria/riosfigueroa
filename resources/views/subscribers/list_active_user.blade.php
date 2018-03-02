e817d5c3a39a6a3c9578d49105f7d952c19f37ae
Jose Canache <canache39@gmail.com>
2018-03-02 16:29:43 -0400

    fix analysis prices added again

--- resources/views/subscribers/list_active_user.blade.php
+++ resources/views/subscribers/list_active_user.blade.php
@@ -33,14 +33,23 @@
                                                         @else Inactivo
                                                         @endif 
                                                     </span>
-                                                  
+                                                    <span id="state-access">
+                                                        @if($user->roles[0]->permissions == 'user_out_mx') - Acceso Global 
+                                                        @else - Acceso solo en México
+                                                        @endif
+                                                    </span>
                                                 </td>
                                                 <td class="table-td-actions">
                                                     @if($user->active == 1) <div class="actions-btn active-user action-desactive" id="{{ $user->id }}" state="0" data_toggle="tooltip" data-placement="bottom" title="Desactivar"><i class="fa fa-times" aria-hidden="true"></i></div>
                                                     @else <div class="actions-btn active-user action-active" id="{{ $user->id }}" state="1" data_toggle="tooltip" data-placement="bottom" title="Activar"><i class="fa fa-check" aria-hidden="true"></i></div>
                                                     @endif
 
-                                    
+                                                    <div class="actions-btn delete-user action-delete" id="{{ $user->id }}" data_toggle="tooltip" data-placement="bottom" title="Eliminar"><i class="fa fa-trash-o" aria-hidden="true"></i></div>
+                                                    
+                                                    @if($user->roles[0]->permissions == 'user_out_mx') <div class="actions-btn global-access active-global-user" id="{{ $user->id }}" state="0" data_toggle="tooltip" data-placement="bottom" title="Desactivar"><i class="fa fa-globe" aria-hidden="true"></i></div>
+                                                    @else <div class="actions-btn global-access desactive-global-user" id="{{ $user->id }}" state="1" data_toggle="tooltip" data-placement="bottom" title="Activar"><i class="fa fa-globe" aria-hidden="true"></i></div>
+                                                    @endif
+
                                                 </td>
                                             </tr>
                                         @endforeach
